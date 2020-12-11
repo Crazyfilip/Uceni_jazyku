040e1e341e555cb0093d8ccf3b2cb503baae599d
sedivy.filip <sedivy.filip@seznam.cz>
2020-12-11 11:43:49 +0100

    ActiveCycleCache

---
+++ Uceni_jazyku/Cycles/ActiveCycleCache/ActiveCycleCache.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace Uceni_jazyku.Cycles
+{
+    public class ActiveCycleCache : IActiveCycleCache
+    {
+        private readonly string activeCycleCacheFile = "cycles/service/active-cycle.xml";
+
+        public void DropCache()
+        {
+            File.Delete(activeCycleCacheFile);
+        }
+
+        public UserCycle GetFromCache()
+        {
+            if (File.Exists(activeCycleCacheFile))
+            {
+                var serializer = new DataContractSerializer(typeof(UserCycle));
+                using XmlReader reader = XmlReader.Create(activeCycleCacheFile);
+                return (UserCycle)serializer.ReadObject(reader);
+            }
+            else
+                return null;
+        }
+
+        public void InsertToCache(UserCycle cycle)
+        {
+            if (cycle.State == UserCycles.UserCycleState.Active)
+            {
+                var serializer = new DataContractSerializer(typeof(UserCycle));
+                using XmlWriter writer = XmlWriter.Create(activeCycleCacheFile);
+                serializer.WriteObject(writer, cycle);
+            }
+            else
+            {
+                throw new ArgumentException("Cycle is in incorrect state to be saved in cache");
+            }
+        }
+
+        public bool IsCacheFilled()
+        {
+            return File.Exists(activeCycleCacheFile);
+        }
+    }
+}
---
+++ Uceni_jazyku/Cycles/ActiveCycleCache/IActiveCycleCache.cs
@@ -0,0 +1,31 @@
+﻿namespace Uceni_jazyku.Cycles
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    interface IActiveCycleCache
+    {
+        /// <summary>
+        /// Empty cache
+        /// </summary>
+        void DropCache();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        UserCycle GetFromCache();
+
+        /// <summary>
+        /// Insert cycle to cache
+        /// </summary>
+        /// <param name="cycle"></param>
+        void InsertToCache(UserCycle cycle);
+
+        /// <summary>
+        /// Test if cache is used
+        /// </summary>
+        /// <returns>true if cache is filled</returns>
+        bool IsCacheFilled();
+    }
+}
